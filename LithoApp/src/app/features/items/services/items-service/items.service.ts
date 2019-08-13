39ad9184fa0d3e52ab4964d7dbdb6455255194d7
FazioNico <contact@nicolasfazio.ch>
2019-08-13 11:45:54 +0200

    upd: add token in request

--- LithoApp/src/app/features/items/services/items-service/items.service.ts
+++ LithoApp/src/app/features/items/services/items-service/items.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { tap, map } from 'rxjs/operators';
 
 @Injectable({
@@ -12,9 +12,18 @@ export class ItemsService {
   ) { }
 
   getItems() {
-    return this._http.get('https://guarded-savannah-51370.herokuapp.com/api/v2/items')
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'x-access-token': (window as any).localStorage.getItem('ionic-demo-auth') || ''
+      })
+    };
+    return this._http.get('https://guarded-savannah-51370.herokuapp.com/api/v2/items', httpOptions)
     .pipe(
-      map((res: {code: number, items: any[]}) => res.items || null),
+      tap((res: {code: number, items: any[], token: string}) => (res.token)
+        ? (window as any).localStorage.setItem('ionic-demo-auth', res.token)
+        : null
+      ),
+      map((res: {code: number, items: any[], token: string}) => res.items || null),
       tap(data => console.log('GET: /items', data))
     );
   }
